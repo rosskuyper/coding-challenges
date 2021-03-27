0fe433fd6f9119f34b6b4fbec6ecc865425c6637
Ross Kuyper <ross@rosskuyper.com>
2021-03-27 07:38:44 -0400

    Add first two already-solved problems

---
+++ leetcode/1-two-sum/solution.ts
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/two-sum/
+ */
+
+function twoSum(nums: number[], target: number): number[] {
+  for (let outerIndex = 0; outerIndex < nums.length; outerIndex++) {
+    for (let innerIndex = 0; innerIndex < nums.length; innerIndex++) {
+      // We can't use the same value twice
+      if (outerIndex === innerIndex) {
+        continue;
+      }
+
+      if (nums[outerIndex] + nums[innerIndex] === target) {
+        // Order does not matter.
+        return [outerIndex, innerIndex];
+      }
+    }
+  }
+
+  throw new Error("No matching solution found.");
+}
---
+++ leetcode/2-add-two-numbers/solution.ts
@@ -0,0 +1,51 @@
+/**
+ * https://leetcode.com/problems/add-two-numbers/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const nodeVal = (node: ListNode | null) => node?.val || 0;
+
+function addRecursive(
+  l1: ListNode | null,
+  l2: ListNode | null,
+  carry: number
+): ListNode | null {
+  // We've reached the end
+  if (!l1 && !l2) {
+    return carry > 0 ? new ListNode(carry, null) : null;
+  }
+
+  const currentVal = nodeVal(l1) + nodeVal(l2) + carry;
+
+  return new ListNode(
+    currentVal % 10,
+    addRecursive(l1?.next, l2?.next, currentVal >= 10 ? 1 : 0)
+  );
+}
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  return addRecursive(l1, l2, 0);
+}
